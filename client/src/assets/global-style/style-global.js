eabc1577338bc3398098444152dc16925833d182
Yago Fogaca <yagofogaca20@gmail.com>
2022-10-14 14:13:14 -0300

    feat: add new font

--- client/src/assets/global-style/style-global.js
+++ client/src/assets/global-style/style-global.js
@@ -1,15 +1,21 @@
-import styled, { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Oswald&display=swap');
 * {
     margin: 0;
     padding: 0;
     outline: 0;
     box-sizing: border-box;
+    font-size: 10px;
+    font-family: 'Oswald', sans-serif;
   }
   html,
   body,
   #root {
     height: 100%;
+  }
+  body{
+    background-color: #EEEEEE;
   }
 `;
