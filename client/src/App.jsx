9913ddb41da5564812aace8d1e21fb51c1419a53
Yago Fogaca <yagofogaca20@gmail.com>
2022-10-14 18:35:01 -0300

    feat: creation and implementation of the home page and the card component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import { Home } from './pages/home/home';
 
 export function App() {
   return (
     <>
       <Header />
+      <Home />
       <Footer />
     </>
   );
