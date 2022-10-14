f735cc620d7bc1b39934b6183e7a9cc7fbabd753
Yago Fogaca <yagofogaca20@gmail.com>
2022-10-14 13:30:04 -0300

    feat: Installation and implementation of styled components

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import GlobalStyle from './assets/global-style/style-global';
 import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
 );
