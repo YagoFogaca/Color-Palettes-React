84d1b0202e6c6d6127952ff057912609c86682d6
Yago Fogaca <yagofogaca20@gmail.com>
2022-10-17 17:57:10 -0300

    feat: Creation and implementation of the route for a single palette

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { Home } from './pages/home/home';
+import { Palette } from './pages/palette/palette';
 
 export function App() {
   return (
     <>
       <Header />
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/palette/:id" element={<Palette />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </>
   );
