d51efa2defffdc543e12d7c803c06ed7e8f5abe8
Yago Fogaca <yagofogaca20@gmail.com>
2022-10-21 14:31:24 -0300

    fix: Try catch implementation api

--- client/src/utils/api/api.js
+++ client/src/utils/api/api.js
@@ -4,9 +4,9 @@ axios.defaults.baseURL = 'https://color-palettes-api.herokuapp.com';
 
 export class api {
   static async getAllPalettes() {
-    const results = await axios.get('/palette/palettes');
-    return results.data;
     try {
+      const results = await axios.get('/palette/palettes');
+      return results.data;
     } catch (err) {
       console.log(err);
     }
