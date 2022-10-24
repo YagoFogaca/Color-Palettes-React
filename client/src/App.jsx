e1bdcebbbdee421e6100c60f6a75add0e4e81a36
Yago Fogaca <yagofogaca20@gmail.com>
2022-10-24 14:00:30 -0300

    feat: Creation and implementation of axios interceptors for requests and responses

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './components/header/header';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
-
+import { Auth } from './utils/api/auth/auth';
 export function App() {
   return (
     <>
       <BrowserRouter>
         <Header />
+        <Auth />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
