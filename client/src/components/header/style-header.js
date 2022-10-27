89cc19a3ff23424812dbf312d894f1655179afdf
Yago Fogaca <yagofogaca20@gmail.com>
2022-10-27 16:39:48 -0300

    feat: added link style

--- client/src/components/header/style-header.js
+++ client/src/components/header/style-header.js
@@ -24,7 +24,41 @@ export const HeaderStyled = styled.header`
     color: #d72323;
   }
 
+  .navbar-link {
+    padding: 1rem;
+    font-size: 2rem;
+    text-decoration: none;
+    color: #fff;
+    transition: all 0.1s;
+    border-bottom: 2px solid #d72323;
+    &:hover {
+      box-shadow: 0px 0px 5px 2px #d72323;
+    }
+  }
+
+  .navbar-link-login {
+    width: 100%;
+    max-width: 100px;
+    min-width: 50px;
+    padding: 0.9rem;
+    background-color: transparent;
+    border: 2px solid #d72323;
+    border-radius: 5px;
+    font-size: 2rem;
+    text-align: center;
+    color: #eeeeee;
+    cursor: pointer;
+    transition: all 0.1s linear;
+    &:hover {
+      box-shadow: 0px 0px 5px 2px #d72323;
+    }
+  }
+
   @media (max-width: 768px) {
+    #navbar {
+      display: none;
+    }
+
     #navbar-mobile,
     #btn-navbar-mobile {
       display: flex;
